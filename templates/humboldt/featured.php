34bd56dd7c8aeb969bae1c58ad655fb37dda1bec
Jared Nipper <jaredn@humboldt.edu>
2011-05-16 14:58:06 -0700

    another featured event format issue

--- templates/humboldt/featured.php
+++ templates/humboldt/featured.php
@@ -6,55 +6,56 @@ function featured() {
     $xmlObj = simplexml_load_string($xmlStr);
     $xmlObj = json_decode(json_encode($xmlObj),1); //convert xml object to array
 
-    $output .= '<div id="featuredEvents">';
-    $output .= '<h2 id="featuredEventsTitle">Featured Events</h2><a></a>';
-    $output .= "<div><div>";
-
-    $events = array_chunk($xmlObj['Event'], 3); // split into groups of 3
-
     // make sure the last row has 3 events
+    $events = array_chunk($xmlObj['Event'], 3); // split into groups of 3
     if (count($events) > 1 && count(end($events)) < 3){   // if the last row has less than 3
         $secondlast = $events[count($events)-2];          // second-to-last row
         $offset = count(end($events));                 // offset index for where to grab from
         $toadd = array_slice($secondlast, $offset); // the array to prepend to last row
         $events[count($events)-1] = array_merge($toadd, $events[count($events)-1]);
-
-        $combined = array();
-        for($x = 0; $x < count($events); $x++) { // merge the groups of 3 back into a single array
-            for($y = 0; $y < 3; $y++) {
-                $combined[] = $events[$x][$y];
-            }
-	}
-        $events = $combined;
     }
 
+    // merge the groups of 3 back into a single array
+    $combined = array();
+    for($x = 0; $x < count($events); $x++) {
+        for($y = 0; $y < 3; $y++) {
+            $combined[] = $events[$x][$y];
+        }
+    }
+    $events = $combined;
+
+    // generate the div
+    $output .= '<div id="featuredEvents">';
+    $output .= '<h2 id="featuredEventsTitle">Featured Events</h2><a></a>';
+    $output .= '<div class="eventsContainer">';
     $count = 1;
     foreach ($events as $event) {
         $time = strtotime($event['DateTime']['StartDate']);
         $formattedTime = date('M j', $time);
 
-        if ($count > 3) {
-            $output .= "</div><div>";
+        if ($count == 1) {
+            $output .= '<div class="setOf3">';
         }
         $output .= '<div class="event_detail">';
 
         if (!empty($event['Images']['Image']['URL'])) {
-            $output .= '<img src="' . $event['Images']['Image']['URL'] . '" alt="' . $event['EventTitle'] . '"
+            $output .= '<img src="' . $event['Images']['Image']['URL'] . '" alt="' . $event['EventTitle'] . '" 
 width"260" height="200" />';
         }
 
         $output .= '<span>' . $formattedTime . '</span>';
-        $output .= '<a href="' . $event['WebPages']['WebPage']['URL'] . '" target="_blank">' .
-$event['EventTitle'] . '</a></div>';
+        $output .= '<a href="' . $event['WebPages']['WebPage']['URL'] . '" target="_blank">' . 
+$event['EventTitle'] . '</a></div><!-- /event_detail -->';
 
-        if ($count > 3) {
+        if ($count == 3) {
+            $output .= '</div><!-- /setOf3 -->';
             $count = 0;
         }
         $count++;
     }
-    $output .= '</div></div></div>';
+    $output .= '</div><!-- /eventsContainer --></div><!-- /featuredEvents-->';
     return $output;
-
 }
 
 ?>
+

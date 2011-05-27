5cd33d6f189b254511fe61cce04681c5336dbf9b
Jared Nipper <jaredn@humboldt.edu>
2011-05-27 13:32:54 -0700

    forgot to add this to previous commit

---
+++ includes/manager/UNL/UCBCN/Manager/Feature.php
@@ -0,0 +1,39 @@
+class UNL_UCBCN_Manager_Feature
+{
+    /**
+     * Management interface to build a featured page for.
+     *
+     * @var UNL_UCBCN_Manager
+     */
+    public $manager;
+    
+    /**
+     * Events to build a featured form for.
+     *
+     * @var array of UNL_UCBCN_Event
+     */
+    //public $events;
+    
+    
+    public function __construct(UNL_UCBCN_Manager &$manager, $events)
+    {
+        $this->manager   = $manager;
+        
+        if (count($events) > 0) {
+            $db = $this->manager->user->getDatabaseConnection();
+            foreach ($events as $event) {
+                $sql = "UPDATE event " . 
+                        "SET status = 'featured' " .
+                        "WHERE id = " . $event->id;
+                $db->query($sql);
+            }
+        } else {
+            return new UNL_UCBCN_Error('No events selected!');
+        }
+        
+        //var_dump($this->manager->calendar->id);
+        $this->manager->localRedirect($this->manager->uri . '?list=posted');
+        exit();
+    }    
+}
+?>

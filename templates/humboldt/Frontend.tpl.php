c3f45e0338f1efa7e788c09dc6149cd6b1c7bd5d
matthodgson <mmh61@humboldt.edu>
2011-06-22 16:34:02 -0700

    back to strict

--- templates/humboldt/Frontend.tpl.php
+++ templates/humboldt/Frontend.tpl.php
@@ -3,7 +3,8 @@ if (!isset($GLOBALS['unl_template_dependents'])) {
 	$GLOBALS['unl_template_dependents'] = $_SERVER['DOCUMENT_ROOT'];
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>

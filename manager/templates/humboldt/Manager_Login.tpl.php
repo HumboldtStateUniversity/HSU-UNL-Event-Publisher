c15eb65ff5403d3d258536e315b4de559f942f42
Jared Nipper <jaredn@humboldt.edu>
2011-07-06 14:03:10 -0700

    Manager login feedback - add error class

--- manager/templates/humboldt/Manager_Login.tpl.php
+++ manager/templates/humboldt/Manager_Login.tpl.php
@@ -2,14 +2,16 @@
 <p>Please log in using your HSU User Name and Password.</p>
 
 <?php
+	$attributes = NULL;
 
         if ($this->status == -3){
-            echo "<p class='error'>Wrong Username or Password</p>";
+            echo "<p class='error'>Your HSU User Name and/or HSU Password were incorrect.</p>";
+	    $attributes = 'class="error"';
         }
 
 	$form = new HTML_QuickForm('event_login', 'post', 'index.php');
-        $form->addElement('text',$this->user_field,'HSU User Name');
-	$form->addElement('password',$this->password_field,'Password');
+        $form->addElement('text',$this->user_field,'HSU User Name', $attributes);
+	$form->addElement('password',$this->password_field,'Password', $attributes);
 	$form->addElement('xbutton','submit','Submit','type="submit"');
 	$renderer = new HTML_QuickForm_Renderer_Tableless();
 	$form->accept($renderer);

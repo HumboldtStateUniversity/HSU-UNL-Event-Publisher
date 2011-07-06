4e923db7054f81cbdd8b93e2238dfeb99ca07854
Jared Nipper <jaredn@humboldt.edu>
2011-07-06 13:28:49 -0700

    Login feedback - template

--- manager/templates/humboldt/Manager_Login.tpl.php
+++ manager/templates/humboldt/Manager_Login.tpl.php
@@ -2,8 +2,13 @@
 <p>Please log in using your HSU User Name and Password.</p>
 
 <?php
+
+        if ($this->status == -3){
+            echo "<p class='error'>Wrong Username or Password</p>";
+        }
+
 	$form = new HTML_QuickForm('event_login', 'post', 'index.php');
-	$form->addElement('text',$this->user_field,'HSU User Name');
+        $form->addElement('text',$this->user_field,'HSU User Name');
 	$form->addElement('password',$this->password_field,'Password');
 	$form->addElement('xbutton','submit','Submit','type="submit"');
 	$renderer = new HTML_QuickForm_Renderer_Tableless();

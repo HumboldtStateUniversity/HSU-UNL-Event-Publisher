2d37e0d15e39c49b5d84ec19f83d79ef2c5a6ea5
Matt Hodgson <mmh61@humboldt.edu>
2011-08-24 09:42:46 -0700

    adding ie 7 specific styles to month calendar grid

--- templates/humboldt/Frontend.tpl.php
+++ templates/humboldt/Frontend.tpl.php
@@ -4,7 +4,12 @@ if (!isset($GLOBALS['unl_template_dependents'])) {
 }
 ?>
 <!DOCTYPE html>
-
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />

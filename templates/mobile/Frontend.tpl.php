8b4b95da924da1e8c4099467f5b4b73a9c4bedac
Melissa Gore <mrg92@humboldt.edu>
2011-09-01 12:54:52 -0700

    mobile template - change doctype

--- templates/mobile/Frontend.tpl.php
+++ templates/mobile/Frontend.tpl.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!doctype html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
 
 <title>Humboldt State <?php
 if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {

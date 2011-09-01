a3aeeda2e5c525f344a1ee0a6e827083e914e7ba
Melissa Gore <mrg92@humboldt.edu>
2011-09-01 12:57:27 -0700

    mobile template - add roles

--- templates/mobile/Frontend.tpl.php
+++ templates/mobile/Frontend.tpl.php
@@ -12,13 +12,22 @@ if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {
 
 <link rel="alternate" type="application/rss+xml" title="<?php echo $this->calendar->name; ?> Events" href="<?php echo UNL_UCBCN_Frontend::formatURL(array('calendar'=>$this->calendar->id,'format'=>'rss')); ?>" />
 </head>
+
+
 <body id="mobilecal">
+		<div data-role="page">
+
+		<div data-role="header" data-title="Humboldt State Events">
+			<h1>Humboldt State Events</h1>			
+			
 		<form id="event_search" name="event_search" method="get" action="<?php echo UNL_UCBCN_Frontend::formatURL(array('calendar'=>$this->calendar->id,'search'=>'search')); ?>">
     <input type='text' name='q' id='searchinput' alt='Search for events' value="<?php if (isset($_GET['q'])) { echo htmlentities($_GET['q']); } ?>" />
     <input type='submit' name='submit' value="Search" />
     <input type='hidden' name='search' value='search' />
 	</form>
+			</div>
     
+		<div data-role="content">
 <?php if (isset($this->right)) { ?>
     <div id="updatecontent" class="three_col right">
     <?php UNL_UCBCN::displayRegion($this->output); ?>
@@ -26,9 +35,10 @@ if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {
 <?php } else {
     UNL_UCBCN::displayRegion($this->output);
 } ?>
+		</div>
 
+    <div data-role="footer"><p>Switch to <a href="<?php echo $this->output[0]->uri; ?>?template=humboldt">standard site</a></p></div>
 
-    <a href="<?php echo $this->output[0]->uri; ?>?template=humboldt">Switch to Full Site</a>
-
+		</div>
 </body>
 </html>

b68765289605b6f9c7ae51e074b4e228ffa84896
Melissa Gore <mrg92@humboldt.edu>
2011-09-01 16:18:40 -0700

    mobile - added css file

--- templates/mobile/Frontend.tpl.php
+++ templates/mobile/Frontend.tpl.php
@@ -11,6 +11,7 @@ if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {
 ?>| Events</title>
 
 		<link rel="stylesheet" type="text/css" media="screen" href="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->uri; ?>templates/mobile/css/main.css" />
 		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->uri; ?>templates/mobile/jqm/jquery.ui.datepicker.mobile.css" />
 		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.2.min.js"></script>

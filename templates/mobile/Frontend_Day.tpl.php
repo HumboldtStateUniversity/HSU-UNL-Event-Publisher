c9668607e82bd7269bf843b2ca7b39e5f2e2aef7
Melissa Gore <mrg92@humboldt.edu>
2011-09-06 15:35:48 -0700

    stray </p>

--- templates/mobile/Frontend_Day.tpl.php
+++ templates/mobile/Frontend_Day.tpl.php
@@ -8,10 +8,9 @@ echo date('l, F jS',$day->getTimeStamp());
 </h4>
 
 
-<div class="day-buttons" data-role="controlgroup" data-type="horizontal">
 <?php
     $prev = $day->prevDay('object');
-    echo '<a data-role="button" data-icon="arrow-l" rel="external" class="url prev" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
+    echo '<div class="day-buttons" data-role="controlgroup" data-type="horizontal"><a data-role="button" data-icon="arrow-l" rel="external" class="url prev" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
                                                             'm'=>$prev->thisMonth(),
                                                             'y'=>$prev->thisYear(),
                                                             'calendar'=>$this->calendar->id)).'">Previous Day</a> ';
@@ -19,10 +18,9 @@ echo date('l, F jS',$day->getTimeStamp());
     echo '<a data-role="button" data-icon="arrow-r" data-iconpos="right" rel="external" class="url next" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$next->thisDay(),
                                                             'm'=>$next->thisMonth(),
                                                             'y'=>$next->thisYear(),
-                                                            'calendar'=>$this->calendar->id)).'">Next Day</a></p>';
+                                                            'calendar'=>$this->calendar->id)).'">Next Day</a></div>';
 
     UNL_UCBCN::displayRegion($this->output);
 
 ?>
-</div>
 </div>

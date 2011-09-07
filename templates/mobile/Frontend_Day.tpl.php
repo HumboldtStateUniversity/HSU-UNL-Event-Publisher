171a00ba94c04686aa97f54570ed051e1837853f
Melissa Gore <mrg92@humboldt.edu>
2011-09-07 16:45:33 -0700

    roles back

--- templates/mobile/Frontend_Day.tpl.php
+++ templates/mobile/Frontend_Day.tpl.php
@@ -1,5 +1,3 @@
-		<div data-role="content">
-
 <div class="calendar clear"></div>
 <div class="day_cal">
 
@@ -14,31 +12,4 @@
 UNL_UCBCN::displayRegion($this->output);
 ?>
 </div>
-		</div>
-		
-		</div>
-    <div data-role="footer" data-position="fixed">
-			<div data-role="navbar">
-				<ul>			
-					<?php
-					
-							$day = new Calendar_Day($this->year,$this->month,$this->day);
-
-					    $prev = $day->prevDay('object');
-					    echo '<li><a rel="external" data-icon="arrow-l" data-iconpos="top" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
-					                                                            'm'=>$prev->thisMonth(),
-					                                                            'y'=>$prev->thisYear(),
-					                                                            'calendar'=>$this->calendar->id)).'">Previous Day</a></li>';
-					    $next = $day->nextDay('object');
-					    echo '<li><a rel="external" data-icon="arrow-r" data-iconpos="top" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$next->thisDay(),
-					                                                            'm'=>$next->thisMonth(),
-					                                                            'y'=>$next->thisYear(),
-					                                                            'calendar'=>$this->calendar->id)).'">Next Day</a></li>';
-
-					?>
-					<li><a href="#two" data-rel="dialog" data-icon="grid" data-iconpos="top">Select Date</a></li>
-					<li><a href="#three" data-rel="dialog" data-icon="search" data-iconpos="top">Search</a></li>
-				</ul>
-			</div>
-			</div>
 		</div>

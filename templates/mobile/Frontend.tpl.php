3a2e9335afafacdd3a106ef687660fa841384cd8
Melissa Gore <mrg92@humboldt.edu>
2011-09-07 12:03:40 -0700

    mobile header

--- templates/mobile/Frontend.tpl.php
+++ templates/mobile/Frontend.tpl.php
@@ -49,14 +49,8 @@ if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {
 		<div data-role="page">
 
 		<div data-role="header" data-title="Humboldt State Events">
-			<h1>Humboldt State Events</h1>
-			</div>
-    
-		<div data-role="content">
+			<h1 class="wordmark"><img src="<?php echo $this->uri; ?>templates/mobile/css/wordmark.png" alt="Humboldt State Events" /></h1>
 			
-
-				<a href="#two" data-role="button" data-rel="dialog">Select a date to view</a>
-
 				<form data-ajax="false" id="event_search" name="event_search" method="get" action="<?php echo UNL_UCBCN_Frontend::formatURL(array('calendar'=>$this->calendar->id,'search'=>'search')); ?>">
 				<div data-role="fieldcontain">	
 		    <input type='search' name='q' id='searchinput' alt='Search for events' value="<?php if (isset($_GET['q'])) { echo htmlentities($_GET['q']); } ?>" />
@@ -64,8 +58,261 @@ if ($this->calendar->id != $GLOBALS['_UNL_UCBCN']['default_calendar_id']) {
 		    <input type='hidden' name='search' value='search' />
 				</div>
 			</form>
+			
+			</div>
+    
+		<div data-role="content">
 
-<?php if (isset($this->right)) { ?>
+				<a href="#two" data-role="button" data-rel="dialog">Select a date to view</a>
+
+<?php if (isset($this->right)) { ?>body { 
+	font-family: Helmet, Freesans, sans-serif;
+}
+img{
+	max-width:100%;
+}
+#date, #date-label{
+	display:none;
+}
+#one .ui-header .ui-title{
+	text-align:left;
+	padding:0;
+	margin:15px 15px 0 15px;
+}
+#one .ui-header .ui-field-contain{
+	margin:10px 15px 18px;
+	padding:0;
+}
+#one .ui-header form{
+	margin:0;
+}
+#one h1 span{
+	border: 0;
+	clip: rect(0 0 0 0);
+	height: 1px;
+	margin: -1px;
+	overflow: hidden;
+	padding: 0;
+	position: absolute;
+	width: 1px;
+}
+.wordmark{
+	width:auto;
+}
+.vevent h3{
+	margin:0;
+}
+.vevent{
+	margin:10px 0;
+	padding:10px 5px;
+}
+.alt{
+	background-color: transparent;
+  background-color: rgba(255, 255, 255, 0.4);
+}
+h4.sec_main {
+  font-size:20px;
+  font-weight: bold;
+	margin:35px 0 0 0;
+}
+.day-buttons a{
+
+}
+a .date, a .location{
+	color:#222;
+	text-decoration:none;
+	font-weight:normal;
+}
+a .summary{
+	text-decoration:underline;
+}
+.ui-field-contain{
+	border-bottom:0;
+}
+
+/* jquery mobile overrides */
+.ui-header {
+	border: 0px solid #bbb;
+	border-bottom:1px solid #bbb;
+	background:url(handmadepaper.png) repeat #6a9231;
+	color: #222;
+	font-weight: bold;
+	text-shadow: 0 -1px 1px #fff;
+	font-family: Helmet, Freesans, sans-serif;
+	-moz-box-shadow: inset 0px -3px 5px rgba(0,0,0,.2);
+	-webkit-box-shadow: inset 0px -3px 5px rgba(0,0,0,.2);
+	box-shadow: inset 0px -3px 5px rgba(0,0,0,.2);
+}
+#two .ui-header{
+	border: 1px solid 		#77a13b;
+	background: 			#6a9231;
+	color: 					#ffffff;
+	font-weight: bold;
+	text-shadow: 0 -1px 1px #416114;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#77a13b), to(#597e24)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #77a13b, #597e24); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #77a13b, #597e24); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #77a13b, #597e24); /* IE10 */
+	background-image:      -o-linear-gradient(top, #77a13b, #597e24); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #77a13b, #597e24);
+	font-family: Helmet, Freesans, sans-serif;
+	-moz-box-shadow: none;
+	-webkit-box-shadow: none;
+	box-shadow: none;
+}
+.ui-content {
+	background:url(exclusive_paper.jpg) repeat #597e24;
+	color: #222;
+	text-shadow: 0 1px 0 	#fff;
+	font-weight: normal;
+	font-family: Helmet, Freesans, sans-serif;
+}
+.ui-content .ui-link, .ui-footer .ui-link{
+	color:#6a9231;
+	text-decoration:none;
+}
+.ui-footer .ui-link{
+	text-decoration:underline;
+}
+.ui-footer{
+	border: 1px solid 		#fff;
+	background: 			#fff;
+	color: 					#222;
+	font-weight: normal;
+	text-shadow: 0 1px 0 	#fff;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#fff)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #fff, #fff); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #fff, #fff); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #fff, #fff); /* IE10 */
+	background-image:      -o-linear-gradient(top, #fff, #fff); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #fff, #fff);
+	padding-left:15px;
+	font-family: Helmet, Freesans, sans-serif;
+}
+
+.ui-shadow-inset {
+	-moz-box-shadow: inset 0px 0px 0px rgba(0,0,0,.2);
+	-webkit-box-shadow: inset 0px 0px 0px rgba(0,0,0,.2);
+	box-shadow: inset 0px 0px 0px rgba(0,0,0,.2);
+}
+.ui-focus{
+	
+}
+.ui-bar-c{
+	border: 1px solid #000;
+	background: 		#fff;
+	color: 					#3E3E3E;
+	font-weight: bold;
+	text-shadow: 0 1px 1px 	#fff;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#fff)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #fff, #fff); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #fff, #fff); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #fff, #fff); /* IE10 */
+	background-image:      -o-linear-gradient(top, #fff, #fff); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #fff, #fff);	
+}
+.ui-header .ui-body-a{
+	color:#222;
+	border-color:#ccc;
+	background-color:#fff;
+}
+/* btn */
+.new-date{
+	margin:0;
+}
+.ui-btn-up-c, .ui-btn-up-a{
+	border: 1px solid #87ac52;
+	background: #6a9231;
+	font-weight: bold;
+	color: #fff;
+	text-shadow: 0 1px 1px #416114;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#87ac52), to(#6a9231)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #87ac52, #6a9231); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #87ac52, #6a9231); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #87ac52, #6a9231); /* IE10 */
+	background-image:      -o-linear-gradient(top, #87ac52, #6a9231); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #87ac52, #6a9231);
+}
+tbody .ui-btn-up-c{
+	border: 1px solid 		#ccc;
+	background: 			#eee;
+	font-weight: bold;
+	color: 					#444;
+	text-shadow: 0 1px 1px #f6f6f6;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#fdfdfd), to(#eee)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #fdfdfd, #eee); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #fdfdfd, #eee); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #fdfdfd, #eee); /* IE10 */
+	background-image:      -o-linear-gradient(top, #fdfdfd, #eee); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #fdfdfd, #eee);	
+}
+tbody .ui-state-highlight{
+	border: 1px solid 		#F7C942;
+	background: 			#fadb4e;
+	font-weight: bold;
+	color: 					#333;
+	text-shadow: 0 1px 0 	#fff;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#fceda7), to(#fadb4e)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #fceda7, #fadb4e); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #fceda7, #fadb4e); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #fceda7, #fadb4e); /* IE10 */
+	background-image:      -o-linear-gradient(top, #fceda7, #fadb4e); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #fceda7, #fadb4e);
+}
+tbody .ui-state-active{
+	border: 1px solid 		#145072;
+	background: 			#2567ab;
+	font-weight: bold;
+	color: 					#fff;
+	text-shadow: 0 -1px 1px #145072;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#5f9cc5), to(#396b9e)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #5f9cc5, #396b9e); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #5f9cc5, #396b9e); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #5f9cc5, #396b9e); /* IE10 */
+	background-image:      -o-linear-gradient(top, #5f9cc5, #396b9e); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #5f9cc5, #396b9e);	
+}
+.ui-btn-up-c a.ui-link-inherit, .ui-btn-up-a a.ui-link-inherit {
+	color: #fff;
+}
+.ui-btn-hover-c, .ui-btn-hover-a {
+	border: 1px solid #bbb;
+	background: #6a9231;
+	font-weight: bold;
+	color: #fff;
+	text-shadow: 0 1px 1px #416114;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#9fc26c), to(#6a9231)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #9fc26c, #6a9231); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #9fc26c, #6a9231); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #9fc26c, #6a9231); /* IE10 */
+	background-image:      -o-linear-gradient(top, #9fc26c, #6a9231); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #9fc26c, #6a9231);
+}
+.ui-btn-hover-c a.ui-link-inherit, .ui-btn-hover-a a.ui-link-inherit {
+	color: #fff;
+}
+.ui-btn-down-c, .ui-btn-down-a {
+	border: 1px solid #bbb;
+	background: #6a9231;
+	font-weight: bold;
+	color: #fff;
+	text-shadow: 0 1px 1px #416114;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#9fc26c), to(#6a9231)); /* Saf4+, Chrome */
+	background-image: -webkit-linear-gradient(top, #6a9231, #6a9231); /* Chrome 10+, Saf5.1+ */
+	background-image:    -moz-linear-gradient(top, #6a9231, #6a9231); /* FF3.6 */
+	background-image:     -ms-linear-gradient(top, #6a9231, #6a9231); /* IE10 */
+	background-image:      -o-linear-gradient(top, #6a9231, #6a9231); /* Opera 11.10+ */
+	background-image:         linear-gradient(top, #6a9231, #6a9231);
+}
+.ui-btn-down-c a.ui-link-inherit, .ui-btn-down-a a.ui-link-inherit {
+	color: #fff;
+}
+.ui-btn-up-c,
+.ui-btn-hover-c,
+.ui-btn-down-c {
+	font-family: Helvetica, Arial, sans-serif;
+	text-decoration: none;
+}
     <div id="updatecontent" class="three_col right">
     <?php UNL_UCBCN::displayRegion($this->output); ?>
     </div>
--- templates/mobile/Frontend_Day.tpl.php
+++ templates/mobile/Frontend_Day.tpl.php
@@ -10,15 +10,15 @@ echo date('l, F jS',$day->getTimeStamp());
 
 <?php
     $prev = $day->prevDay('object');
-    echo '<div class="day-buttons" data-role="controlgroup" data-type="horizontal"><a data-role="button" data-icon="arrow-l" rel="external" class="url prev" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
+    echo '<div class="day-buttons" data-role="controlgroup" data-type="horizontal"><a data-role="button" data-icon="arrow-l" data-iconpos="notext" rel="external" class="url prev" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
                                                             'm'=>$prev->thisMonth(),
                                                             'y'=>$prev->thisYear(),
-                                                            'calendar'=>$this->calendar->id)).'">Previous Day</a> ';
+                                                            'calendar'=>$this->calendar->id)).'">Previous</a> ';
     $next = $day->nextDay('object');
     echo '<a data-role="button" data-icon="arrow-r" data-iconpos="right" rel="external" class="url next" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$next->thisDay(),
                                                             'm'=>$next->thisMonth(),
                                                             'y'=>$next->thisYear(),
-                                                            'calendar'=>$this->calendar->id)).'">Next Day</a></div>';
+                                                            'calendar'=>$this->calendar->id)).'">Next</a></div>';
 
     UNL_UCBCN::displayRegion($this->output);
 

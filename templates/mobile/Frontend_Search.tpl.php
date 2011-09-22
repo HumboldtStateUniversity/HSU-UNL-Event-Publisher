80c66de7b2627c32234b17e7bf7d3822d55ed3fa
Melissa Gore <mrg92@humboldt.edu>
2011-09-22 15:59:54 -0700

    mobild fix hidden search results

--- templates/mobile/Frontend_Search.tpl.php
+++ templates/mobile/Frontend_Search.tpl.php
@@ -2,11 +2,11 @@
 <?php
 if (is_a($this->output,'UNL_UCBCN_EventListing')) {
     if ($dt = strtotime($this->query)) {
-        echo '<h1 class="results">Results for events dated <span>'.date('F jS',$dt).'</span></h1>';
+        echo '<h2 class="results">Results for events dated <span>'.date('F jS',$dt).'</span></h2>';
     } elseif (isset($this->eventtype)) {
-        echo '<h1 class="results">Results for <span>' . $this->eventtype_name[0] . '</span></h1>';
+        echo '<h2 class="results">Results for <span>' . $this->eventtype_name[0] . '</span></h2>';
     } else {
-        echo '<h1 class="results">Results for "<span>'.htmlentities($this->query).'</span>"</h1>';
+        echo '<h2 class="results">Results for "<span>'.htmlentities($this->query).'</span>"</h2>';
     }
     echo '<h3 class="result-count">'.count($this->output->events).' results</h3>';
 }

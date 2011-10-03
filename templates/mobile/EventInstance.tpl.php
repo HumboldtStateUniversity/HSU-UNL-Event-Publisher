156128518962f49b83ca871a20e65ad99cc7c13c
Melissa Gore <mrg92@humboldt.edu>
2011-10-03 11:17:47 -0700

    mobile event listing

--- templates/mobile/EventInstance.tpl.php
+++ templates/mobile/EventInstance.tpl.php
@@ -7,7 +7,7 @@ $endu = strtotime($this->eventdatetime->endtime);
 <div class="event_cal">
 <div class='vcalendar'>
 	<div class='vevent'>
-		<h1 class='summary'><a rel="external" href="<?php echo $this->uri; ?>"><?php echo date('l, F jS',$startu); ?></a></h1>
+		<h1 class='summary'><a rel="external" href="<?php echo UNL_UCBCN_Frontend::dbStringToHtml($this->event->date); ?>"><?php echo date('l, F jS',$startu); ?></a></h1>
 		<!--<div id="tabsG">
 		  <ul>
 		    <li><a href="#" id="event_selected" title="Event Detail"><span>Event Detail</span></a></li>

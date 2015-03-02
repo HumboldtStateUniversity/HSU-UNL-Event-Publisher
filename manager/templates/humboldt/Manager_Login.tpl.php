8c58262b9e1ddda09080e8f79434e9796ffb6496
Matt Hodgson <mmh61@humboldt.edu>
2015-03-02 13:19:17 -0800

    changing promote event link to new wordout page on marcom site

--- manager/templates/humboldt/Manager_Login.tpl.php
+++ manager/templates/humboldt/Manager_Login.tpl.php
@@ -5,8 +5,8 @@
 		<li>The event must be sponsored by Humboldt State University, department, program, student club or organization.</li>
 		<li>The event listing must include an event title, date, time, location, contact person, and admission fee (if applicable).</li>
 		<li>Events will be moderated by HSU staff</li>
-		<li>Don't meet the requirements? <a href="http://humboldt.edu/marcom/campusResources.php?section=eventPromo">Learn more about promoting your event</a></li>
-	</ul>         	
+		<li>Don't meet the requirements? <a href="http://humboldt.edu/marcom/campusResources.php?section=promote">Learn more about promoting your event</a></li>
+	</ul>
 </div>
 <div class="login">
 <p>Please log in using your HSU User Name and Password.</p>
